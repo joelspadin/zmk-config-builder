b7391d267a47e9d99d65e2e708525f4b425cbc40
Joel Spadin <joelspadin@gmail.com>
2021-07-12 20:56:17 -0500

    Fix vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,37 @@
 import cdn from '@vitejs/plugin-cdn';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import * as rollup from 'rollup';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        reactRefresh(),
-        // Vite really doesn't like any way of polyfilling node builtins, so
-        // load the browser versions of these modules from a CDN.
-        cdn('skypack', {
-            '@octokit/rest': '^18.6.6',
-            'isomorphic-git': '^1.8.3',
-        }),
-    ],
+export default defineConfig(({ command }) => {
+    const plugins = [reactRefresh()];
+    const rollupPlugins: rollup.Plugin[] = [];
+
+    switch (command) {
+        case 'build':
+            rollupPlugins.push(nodePolyfills());
+            break;
+
+        case 'serve':
+            plugins.push(
+                // Polyfilling node builtins doesn't work when serving, so
+                // load the browser versions of these modules from a CDN.
+                cdn('skypack', {
+                    '@octokit/rest': '^18.6.6',
+                    'isomorphic-git': '^1.8.3',
+                }),
+            );
+            break;
+    }
+
+    return {
+        plugins,
+        build: {
+            rollupOptions: {
+                plugins: rollupPlugins,
+            },
+        },
+    };
 });
